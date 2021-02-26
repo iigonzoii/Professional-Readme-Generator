75f0c8ca8c9e9f19320b686dedd2e1fa7a9e076b
iigonzoii <a.gonzalez418@gmail.com>
2021-02-26 16:12:24 -0700

    rough draft of questions array

--- Develop/index.js
+++ Develop/index.js
@@ -3,6 +3,77 @@ const inquirer = require(`inquirer`)
 const fs = require(`fs`)
 const util = require('util')
 
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'please enter a description of your project',
+      },
+      {
+        type: 'input',
+        name: 'table',
+        message: 'table of contents placeholder, will return here',
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'What is the installation process?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usuage of this project?',
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'What is the installation process?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message:`What license will you be using for this project?`,
+        choices: [
+          "Apache",
+          "MIT",
+          "Mozilla",
+          "GNU GPLv3"
+        ]
+      },
+      {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who are the contributors if any?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'are there any tests included?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+    ]);
+  };
+
+// ****// const writeFileAsync = util.promisify(fs.writeFile);*****
+
+
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
